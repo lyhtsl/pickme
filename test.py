949b36006c4ce627bec564d0a2f209139436273b
lyhtsl <54989887+lyhtsl@users.noreply.github.com>
2019-12-03 23:21:54 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -10,8 +10,8 @@ from time import sleep
 class PYSQL(object):
 
 
-    def __init__(self, host1, server1,port1,user1, password1, database1):
-        self.conn = pymssql.connect(host=host1, server=server1,port=port1,user=user1, password=password1, database=database1)
+    def __init__(self, host1, user1, password1, database1):
+        self.conn = pymssql.connect(host1, user1, password1,database1)
         self.cursor = self.conn.cursor()
 
     def insert_date(self, sql):
@@ -61,6 +61,197 @@ order_number = 1
 # 反馈实时位置
 # 查询订单状态
 
+#短租车查询未完成订单（type为0是租客，为1是车主）
+@app.route('/rentcar/finish')
+def getRentcar_finish():
+    username = request.args.get('user')
+    type = request.args.get('type')
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
+
+    if int(type) == 1:
+        sql = "select * from orders where finish is NULL and dusername='{}'".format(username)
+        result = my.select_data(sql)
+        # print(result)
+        if result:
+            status = 1
+            data = []
+            if result[0][1]:
+
+                msg = '你有未完成订单'
+                dd = result[0]
+                sql1 = "select sname,stel from student where susername='{}'".format(dd[1])
+                dn = my.select_data(sql1)
+                print(dn)
+                if dn:
+                    data = {"oid": dd[0], "sname": dn[0][0] ,"stel": dn[0][1],"address": dd[7],"sdate": dd[9],
+                            "edate": dd[11], "cycle":dd[14],"price": dd[13] }
+            else:
+                msg = '你的订单待匹配'
+                dd = result[0]
+                data = {"oid": dd[0],  "address": dd[7], "sdate": dd[9],
+                        "edate": dd[11], "cycle": dd[14], "price": dd[13]}
+            res = {
+                "status": status,
+                "msg": msg,
+                "data": data
+            }
+        else:
+            status = -1
+            msg = "没有进行中订单"
+            res = {
+                "status": status,
+                "msg": msg,
+            }
+    else:
+        sql = "select * from orders where finish is NULL and susername='{}'".format(username)
+        result = my.select_data(sql)
+        print(result)
+        if result:
+            status = 1
+            msg = '你有未完成订单'
+            dd = result[0]
+            sql1 = "select dname,dtel from driver where dusername='{}'".format(dd[2])
+            dn = my.select_data(sql1)
+            print(dn)
+            data = {"oid": dd[0], "dname": dn[0][0], "dtel": dn[0][1], "address": dd[7], "sdate": dd[9], "edate": dd[11], "cycle": dd[14], "price": dd[13]}
+            res = {
+                "status": status,
+                "msg": msg,
+                "data": data
+            }
+        else:
+            status = -1
+            msg = "没有进行中订单"
+            res = {
+                "status": status,
+                "msg": msg,
+            }
+    my.close()
+    return json.dumps(res, ensure_ascii=False)
+
+# passager get driver rentcar data 乘客获取可租信息
+@app.route('/rentcar/data')
+def getRentcar_data():
+    sdate = request.args.get("sdate")
+    edate = request.args.get("edate")
+    data = []
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
+    sql = "SELECT orderid,dusername,startpoint,cycle ,price FROM orders WHERE susername is NULL AND rent_car = 1 AND  sdate = '{}' AND edate = '{}'".format(sdate, edate)
+    data1 = my.select_data(sql)
+    for vi in data1:
+        sql = "SELECT dname,dtel FROM driver WHERE dusername = '{}'".format(vi[1])
+        data2 = my.select_data(sql)
+        data3 = data2[0]
+        data4 = {"oid": vi[0], "address": vi[2], "cycle": vi[3], "price": vi[4], "name": data3[0], "tel": data3[1]}
+        data.append(data4)
+
+
+    '''
+    requery database
+    '''
+    my.close()
+    res = {
+        "status": 0,#0请求成功
+        "data": data
+    }
+    return json.dumps(res)
+
+# passager take rent car order 乘客租车
+@app.route('/driver/rentcar/tkod')
+def takeRentOrder():
+    oid = request.args.get("oid")
+    user = request.args.get("user")
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
+    sql = "UPDATE orders SET susername ='{}' WHERE orderid = '{}'".format(user,oid)
+    my.update_data(sql)
+    my.close()
+    '''
+    updata database
+    '''
+    res = {
+        "status": 0,
+        "msg": "seccess",
+        "data": {
+
+        }
+    }
+    return json.dumps(res)
+
+
+# passager release rent car order 车主发布租车订单
+@app.route('/rentcar/release')
+def releaseRentar():
+    did = request.args.get("did")
+    sdate = request.args.get("sdate")
+    #stime = request.args.get("stime")
+    edate = request.args.get("edate")
+    #etime = request.form.get("etime")
+    adress = request.args.get("adr")
+    price = request.args.get("price")
+    cycle = request.args.get("cycle")
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
+    sql = "INSERT INTO orders(dusername,rent_car,sdate,edate,startpoint,price,cycle) VALUES ('{}',1 ,'{}','{}','{}','{}','{}')".format(did, sdate, edate, adress, price, cycle)
+    my.insert_date(sql)
+    sql = "SELECT orderid FROM orders WHERE dusername = '{}' AND rent_car=1 AND sdate = '{}' AND edate = '{}' AND startpoint = '{}' AND price = '{}' AND cycle ='{}'".format(did, sdate, edate, adress, price, cycle)
+    data1 = my.select_data(sql)
+    data = data1[0]
+    my.close()
+
+    '''
+    add into database
+    '''
+    res = {
+        "status": 0,
+        "msg": "success",
+        "oid": data[0]
+    }
+    return json.dumps(res)
+
+# owner take rent car order 乘客接出租订单并返回乘客信息
+@app.route('/driver/rentcar/tkod', methods=['POST'])
+def takerentdata():
+    oid = request.args.get("oid")
+    driver_id = request.args.get("did")
+    '''
+    updata database
+    '''
+    res = {
+        "status": 0,
+        "data": {
+            "msg": "seccess"
+        }
+    }
+    return json.dumps(res)
+
+# owner get tail wind car order data 车主获取订单数据
+@app.route('/driver/rentcar/data')
+def getRent_data():
+    oid = request.args.get("oid")
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
+    sql ="SELECT susername FROM orders WHERE orderid = '{}'".format(oid)
+    data1 = my.select_data(sql)
+    data2 = data1[0]
+    print(data2)
+    if data2[0] == None:
+        res = {
+            "status": 0,
+            "msg": "Nobody rents this car"
+        }
+
+    else:
+        sql ="SELECT sname,ssex,stel FROM student WHERE susername = '{}'".format(data2[0])
+        data3 = my.select_data(sql)
+        data4 = data3[0]
+        data5 = {"name": data4[0], "sex": data4[1], "tel": data4[2]}
+        res = {
+            "status": 0,
+            "msg": "seccess",
+            "data": data5
+        }
+
+    my.close()
+    return json.dumps(res)
+
 # passager get driver data 乘客获取司机信息
 @app.route('/quikcar/data')
 def getquikcar_data():
@@ -68,7 +259,7 @@ def getquikcar_data():
     '''
     requery database
     '''
-    my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
     sql = "SELECT dusername FROM orders WHERE orderid = '{}'".format(oid)
     did1 = my.select_data(sql)
     did2 = did1[0]
@@ -94,10 +285,12 @@ def getquikcar_data():
 def takeQcOrder():
     oid = request.args.get("oid")
     driverid = request.args.get("did")
+    print(oid)
+    print(driverid)
     '''
     updata database
     '''
-    my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
     sql = "UPDATE orders SET dusername ='{}' WHERE orderid ='{}'".format(driverid, oid)
     my.update_data(sql)
     sql = "SELECT susername FROM orders WHERE orderid = '{}'".format(oid)
@@ -133,7 +326,7 @@ def releaseQcar():
     '''
     add into database
     '''
-    my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
     sql = "INSERT INTO orders(susername,fast_car,startpoint,endpoint,sdate,stime) VALUES('{}',1,'{}','{}','{}','{}')".format(username,startPlace,destination, date,time)
     my.insert_date(sql)
     sql =  "SELECT orderid FROM orders WHERE susername='{}' AND startpoint ='{}' AND endpoint = '{}'AND sdate ='{}'AND stime ='{}'".format(username, startPlace, destination, date,time)
@@ -160,8 +353,8 @@ def getTwc_data():
     '''
     requery database
     '''
-    if order ==1:
-        my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+    if order[0] =='1':
+        my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
         sql = "SELECT orderid,susername,startpoint,endpoint FROM orders WHERE dusername is NULL AND fast_car = 1 AND  sdate = '{}' AND stime BETWEEN '{}' AND '{}'".format(
             date, time1, time2)
         data1 = my.select_data(sql)
@@ -174,7 +367,7 @@ def getTwc_data():
                      "age": data3[2]}
             data.append(data4)
     else:
-        my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+        my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
         sql = "SELECT orderid,susername,startpoint,endpoint FROM orders WHERE dusername is NULL AND free_ride = 1 AND  sdate = '{}' AND stime BETWEEN '{}' AND '{}'".format(
             date, time1, time2)
         data1 = my.select_data(sql)
@@ -190,6 +383,7 @@ def getTwc_data():
         "status": 0,
         "data": data
     }
+    my.close()
     return json.dumps(res, ensure_ascii=False)
 # driver take tail wind car order 司机接顺风车订单并返回乘客信息
 @app.route('/driver/twc/tkod', methods=['POST'])
@@ -199,7 +393,7 @@ def takeTwcOrder():
     '''
     updata database
     '''
-    my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
     sql = "UPDATE orders SET dusername ='{}' WHERE orderid ='{}'".format(driverid,oid)
     my.update_data(sql)
     sql = "SELECT susername FROM orders WHERE orderid = '{}'".format(oid)
@@ -233,7 +427,7 @@ def releaseTwcOrder():
     '''
     add into database
     '''
-    my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
     sql = "INSERT INTO orders(susername,free_ride,startpoint,endpoint,sdate,stime) VALUES('{}',1,'{}','{}','{}','{}')".format(username,startPlace,destination, date,time)
     my.insert_date(sql)
     sql =  "SELECT orderid FROM orders WHERE susername='{}' AND startpoint ='{}' AND endpoint = '{}'AND sdate ='{}'AND stime ='{}'".format(username, startPlace, destination, date,time)
@@ -262,7 +456,7 @@ def refund():
     '''
     query database
     '''
-    my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
     sql = "SELECT deposit FROM orders WHERE orderid ='{}'".format(oid)
     deposit1 =my.select_data(sql)
     sql = "UPDATE orders SET deposit=0 WHERE orderid ='{}'".format(oid)
@@ -295,7 +489,7 @@ def query_deposit():
     query database
     '''
     if deposit:
-        my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+        my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
         sql ="UPDATE orders SET deposit={} WHERE orderid ='{}'".format(deposit, oid)
         my.update_data(sql)
         my.close()
@@ -321,7 +515,7 @@ def deposit():
     '''
     add into database
     '''
-    my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+    my = PYSQL('127.0.0.1', 'sa', 'lin123456.', 'pickme')
     sql = "SELECT deposit FROM orders WHERE orderid ='{}'".format(oid)
     data1 = my.select_data(sql)
     my.close()
@@ -377,31 +571,56 @@ def get_info():
     '''
     query database
     '''
-    con = pymssql.connect('127.0.0.1', 'test', '123', 'pickme')
-    cursor = con.cursor()
+    my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
     # print(cursor)
     sql = "SELECT username FROM users where username='{}'".format(username)
-    cursor.execute(sql)
-    res = cursor.fetchone()
+    res = my.select_data(sql)
 
     if res == None:
         msg = 'can not find the user'
     else:
-        sql = "SELECT * FROM users where username='{}'".format(username)
-        cursor.execute(sql)
-        result = cursor.fetchone()
+        sql = "SELECT dusername FROM driver where dusername='{}'".format(username)
+        res = my.select_data(sql)
+        if res ==None:
+            sql = "SELECT stel,userid,sname,ssex,sbirth FROM student where susername='{}'".format(username)
+            suer1 = my.select_data(sql)
+            suer = suer1[0]
+            res = {
+                "status": 0,
+                "data": {
+                    "msg": "seccess",
+                    "username": username,
+                    "tel": suer[0],
+                    "userid": suer[1],
+                    "name": suer[2],
+                    "sex": suer[3],
+                    "birth": suer[4]
+                }
+            }
+        else:
+            sql = "SELECT dtel,userid,dname,dsex,dbirth FROM driver where dusername='{}'".format(username)
+            suer1 = my.select_data(sql)
+            suer = suer1[0]
+            res = {
+                "status": 0,
+                "data": {
+                     "msg": "seccess",
+                     "username": username,
+                     "tel": suer[0],
+                     "userid": suer[1],
+                     "name": suer[2],
+                     "sex": suer[3],
+                     "birth": suer[4]
+                }
+            }
+
         '''
         return result
         '''
         msg = 'success'
-    cursor.close()
-    res = {
-        "status": 0,
-        "data": {
-            "msg": msg
-        }
-    }
-    return json.dumps(res)
+        my.close()
+
+    return json.dumps(res, ensure_ascii=False)
 
 
 # register 注测
@@ -475,6 +694,69 @@ def login():
         }
         return json.dumps(res)
 
+@app.route('/requery') # type=0:p?driver
+def isoddone():
+    username = request.args.get('user')
+    type = request.args.get('type')
+    my = PYSQL('127.0.0.1', 'DESKTOP-Q15FB3G\SQLEXPRESS', '55007', 'sa', 'lin123456.', 'pickme')
+
+    if int(type) == 0:
+        sql = "select * from orders where finish is NULL and susername='{}'".format(username)
+        result = my.select_data(sql)
+        # print(result)
+        if result:
+            status = 1
+            data = []
+            if result[0][2]:
+
+                msg = '你有未完成订单'
+                dd = result[0]
+                sql1 = "select dname,dtel,cycleinfo from driver where dusername='{}'".format(dd[2])
+                dn = my.select_data(sql1)
+                print(dn)
+                if dn:
+                    data = {"oid":dd[0],"dname":dn[0][0], "st": dd[7], "isfast": dd[3], "isfree": dd[4], "end": dd[8], "dtel":dn[0][1], "cycleinfo":dn[0][2]}
+            else:
+                msg = '你的订单待匹配'
+
+            res = {
+                "status": status,
+                "msg": msg,
+                "data": data
+            }
+        else:
+            status = -1
+            msg = "没有进行中订单"
+            res = {
+                "status": status,
+                "msg": msg,
+            }
+    else:
+        sql = "select * from orders where finish is NULL and dusername='{}'".format(username)
+        result = my.select_data(sql)
+        print(result)
+        if result:
+            status = 1
+            msg = '你有未完成订单'
+            dd = result[0]
+            sql1 = "select sname,stel from student where susername='{}'".format(dd[1])
+            dn = my.select_data(sql1)
+            print(dn)
+            data = {"oid": dd[0], "sname": dn[0][0], "st": dd[7], "isfast": dd[3], "isfree": dd[4], "end": dd[8], "stel":dn[0][1]}
+            res = {
+                "status": status,
+                "msg": msg,
+                "data": data
+            }
+        else:
+            status = -1
+            msg = "没有进行中订单"
+            res = {
+                "status": status,
+                "msg": msg,
+            }
+    my.close()
+    return json.dumps(res, ensure_ascii=False)
 
 @app.route('/',methods=['GET', 'POST'])
 def hello_world():
